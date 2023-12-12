1d17a521194c98d4fd2b300c5cef3d394bd8254a
m1ker1n <m1ker1nigor@yandex.ru>
2023-12-12 14:36:00 +0300

    added randomFormat

--- greetings.go
+++ greetings.go
@@ -3,11 +3,22 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
 
 func Hello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("empty name")
 	}
-	return fmt.Sprintf("Hi, %s. Welcome!", name), nil
+	return fmt.Sprintf(randomFormat(), name), nil
+}
+
+func randomFormat() string {
+	formats := []string{
+		"Hi, %s. Welcome!",
+		"Great to see you, %s!",
+		"Hail, %s! Well met!",
+	}
+
+	return formats[rand.Intn(len(formats))]
 }
